13d19007553e0458cb6ac08cc3eccbae4286ade6
2-kris <misaalkrishna@gmail.com>
2026-06-05 15:53:05 +0530

    changed ami id

--- web/main.tf
+++ web/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "server" {
-  ami           = "ami-029a761f237195c2c"
+  ami           = "ami-0d13e2317a7e75c95"
   instance_type = "t2.micro"
   subnet_id     = var.sn
   security_groups = [var.sg]
