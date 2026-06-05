8bbc84b4a1518273beaebb9c32fb34599174ae91
2-kris <misaalkrishna@gmail.com>
2026-06-05 16:24:35 +0530

    subnet az

--- vpc/main.tf
+++ vpc/main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "myvpc" {
 resource "aws_subnet" "mysubnet" {
   vpc_id     = aws_vpc.myvpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
+  availability_zone = "us-west-2a"
   tags = {
     Name = "mysubnet"
   }
